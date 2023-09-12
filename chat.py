1d267249798db7184b21b521dc389c5df19dbde8
sbrener1610 <sbrener1610@gmail.com>
2023-09-12 04:45:10 +0200

    prompt automatization, bug fix

--- chat.py
+++ chat.py
@@ -68,9 +68,9 @@ class BusinessIdeasChat:
         self._make_system_role() #new system role with new expert
         self.conversation[0]["content"]=self.system_role
         if len(coords)==1:
-            prompt=f"You are the best {self.__expert} in the whole world, with 20 years experience. I am your client and i want to start {cur_dialog_step['title']} in the Germany. Describe in detail and write what is needed to start. No need to mention the current year(2021) in the answer. Write strictly in the format: number. #name of the idea# $the area expert$ - |list in imperative form for the second person| Use only these seperators:#$| do not write an introduction or a conclusion"
+            prompt=f"You are the best {self.__expert} in the whole world, with 20 years experience. I am your client and i want to start {cur_dialog_step['title']} in the Germany. Describe in detail and write what is needed to start. No need to mention the current year(2021) in the answer. Write strictly in the format: number. #name of the idea# $the area expert$ - |business action in imperative form for the second person| Use only these seperators:#$| do not write an introduction or a conclusion"
         else:
-            prompt=f"Your current choice of business action is {cur_dialog_step['title']}. Imagine it's September 2021. you're the best {self.__expert} in the whole world, with 20 years of experience. I'm your client and i want you to fetch me a detailed report corresponding to this business action using the following information.\n {cur_dialog_step['content']} \nNo need to mention the current year in the answer.\nWrite strictly in the format: number. #name of the idea# $the area expert$ - |list in imperative form for the second person| Use only these seperators:#$| do not write an introduction or a conclusion"
+            prompt=f"Your current choice of business action is {cur_dialog_step['title']}. Imagine it's September 2021. you're the best {self.__expert} in the whole world, with 20 years of experience. I'm your client and i want you to fetch me a detailed report corresponding to this business action using the following information.\n {cur_dialog_step['content']} \nNo need to mention the current year in the answer.\nWrite strictly in the format: number. #name of the idea# $the area expert$ - |business action in imperative form for the second person| Use only these seperators:#$| do not write an introduction or a conclusion"
         
         self.conversation.append({"role": "user", "content": prompt})
         self.response = openai.ChatCompletion.create(
@@ -80,12 +80,19 @@ class BusinessIdeasChat:
         self.assistant_message=self.response['choices'][0]['message']['content']
         self.__append_database(coords,prompt,self._parse_response()) # adds prompt and response to the database
 
-    def __append_database(self,coords,prompt,elaborate):
-        if len(coords)==1:
-            self.__database[coords[0]]["prompt"]=prompt
-            self.__database[coords[0]]["elaborate"]=elaborate
-        else:
-            self.__append_database(coords[1:],prompt,elaborate)
+    def __append_database(self, coords, prompt, elaborate):
+        
+        def append_database(data, coords, prompt, elaborate):
+            if len(coords)==1:
+                data[coords[0]]["prompt"]=prompt
+                data[coords[0]]["elaborate"]=elaborate
+            else:
+                append_database(data[coords[0]]["elaborate"],coords[1:],prompt,elaborate)
+            return data
+        
+        data=self.__database
+        self.__database=append_database(data, coords, prompt, elaborate)
+        
             
 
 
